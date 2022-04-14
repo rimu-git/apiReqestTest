bd1c88d8f5252af60a06d284a5762f9c175364a3
r.mukai <****@gmail.com>
2022-04-15 01:27:46 +0900

    誕生日入れたい

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # apiReqestTest
+
+## 要件定義
+・生年月日検索
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 import json
+from datetime import datetime
 
 res = requests.get(
-    'https://api.dmm.com/affiliate/v3/ActressSearch?api_id=pZKdCGmsWwwYMgckK1yX&affiliate_id=harapekoafi-999&gte_birthday=2001-01-01&sort=birthday&hits=100&offset=1&output=json')
+    'https://api.dmm.com/affiliate/v3/ActressSearch?api_id=pZKdCGmsWwwYMgckK1yX&affiliate_id=harapekoafi-999&gte_hip=95&sort=-birthday&hits=100&offset=1&output=json')
 
 # print(res.status_code)
 # print(res.text)
@@ -19,20 +20,61 @@ results_actress = res_json['result']['actress']
 # 件数を取得
 results_actress_count = len(results_actress)
 
-print('取得件数：' + results_actress_count.__str__())
+print('取得件数：' + str(results_actress_count))
 
+savedCount = 0
 # 配列から取得
 for i in results_actress:
-    print('女優名：' + i['name'], '誕生日：' + i['birthday'], 'サムネ：' + i['imageURL']['large'],
-          '動画：' + i['listURL']['digital'])
 
-    # サムネイルを保存
-    url = i['imageURL']['large']
-    file_name = "C:\\Users\\harap\\Pictures\\" + i['name'] + ".jpg"
+    if i['name'] is None:
+        actName = ''
+    else:
+        actName = i['name']
 
-    response = requests.get(url)
-    image = response.content
+    if i['birthday'] is None:
+        birDate = ''
+    else:
+        # today = datetime.date.today()
+        birDate = i['birthday']
+        # bd = i['birthday']
+        # tdatetime = datetime.strptime(bd, '%Y-%m-%d')
+        # tdate = datetime.date(tdatetime.year, tdatetime.month, tdatetime.day)
+        # birthday = datetime.date(tdate)
+        # birDate= (int(today.strftime("%Y%m%d")) - int(birthday.strftime("%Y%m%d"))) // 10000
 
-    with open(file_name, "wb") as aaa:
-        aaa.write(image)
+    if i['hip'] is None:
+        hipSize = ''
+    else:
+        hipSize = i['hip']
 
+    if 'imageURL' in i:
+        imageLink = i['imageURL']['large']
+
+        # サムネイルを保存
+        file_name = "C:\\Users\\harap\\Pictures\\Camera Roll\\test\\" + actName + ".jpg"
+
+        response = requests.get(imageLink)
+        image = response.content
+
+        with open(file_name, "wb") as aaa:
+            aaa.write(image)
+            savedCount += 1
+
+    else:
+        imageLink = ''
+
+    if 'listURL' in i:
+        videoLink = i['listURL']['digital']
+    else:
+        videoLink = ''
+
+    print('女優名：' + actName, '誕生日：' + birDate, 'ヒップサイズ：' + hipSize, '画像：' + imageLink,
+          '動画：' + videoLink)
+
+    actName = ''
+    birDate = ''
+    hipSize = ''
+    imageLink = ''
+    videoLink = ''
+
+print(str(savedCount) + '／' + str(results_actress_count) + 'の画像保存　処理が完了しました')
